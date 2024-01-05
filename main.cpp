189fc3f9234e7dc9f0c4926375e81550111012c4
Tonybesto <akojianthony@gmail.com>
2024-01-05 08:23:57 +0100

    Add code

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <iostream>
+
+int main() {
+    // Print the message "Hello, C++!" to the console
+    std::cout << "Hello, C++!" << std::endl;
+
+    // Indicate successful execution and return 0
+    return 0;
+}
