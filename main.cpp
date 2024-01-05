7b1a9897844b978e869350aac908415c3c1b0d70
Tonybesto <akojianthony@gmail.com>
2024-01-05 15:35:43 +0100

    update cpp-check.yml

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
 int main() {
+    int x;
+    int y = x * 2;  // Using uninitialized variable x
+
+    // Print the value of y
+    std::cout << "Value of y: " << y << std::endl;
+
+    // Indicate successful execution and return 0
+    return 0;
+}#include <iostream>
+
+int main() {
     // Print the message "Hello, C++!" to the console
     std::cout << "Hello, C++!" << std::endl;
 
